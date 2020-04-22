eb015239e65490599333aa39521bb339fc9f3613
Edward Yong Gyun Kim <edward@edykim.com>
2020-04-22 13:06:39 -0700

    Add Korean text

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,6 +8,7 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
+    <p>Korean: 여기도 한글로 작성된 부분입니다. 안녕하세요~</p>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
