83b6b5064406bc060367590f7a4e0d90f2bdf263
Edward Yong Gyun Kim <edward@edykim.com>
2020-04-22 11:10:51 -0700

    Add redirection fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,21 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const fs = require("fs")
+const path = require("path")
+
+exports.onPreBootstrap = ({ store }) => {
+  const { program } = store.getState()
+  const cacheDirectory = `${program.directory}/.cache`
+  const appFile = path.join(cacheDirectory, "production-app.js")
+  const code = fs.readFileSync(appFile, {
+    encoding: `utf-8`,
+  })
+
+  const newCode = code.replace(
+    "pagePath &&",
+    "window.parent.location === browserLoc && pagePath &&"
+  )
+
+  fs.writeFileSync(appFile, newCode, `utf-8`)
+}
