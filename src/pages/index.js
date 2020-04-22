8b757f96716457e3ec6881b635924dc07ef4a499
Edward Yong Gyun Kim <edward@edykim.com>
2020-04-22 12:23:29 -0700

    Add Korean text

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Korean: 이 부분은 한국어로 작성되어 있습니다.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
